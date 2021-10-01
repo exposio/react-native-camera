df85daf4c4ce6ac937a02ae75687dc77bf27c18a
Jonathan Gilbert <jonathan.gilbert@exposiohdr.com>
2021-10-01 11:01:37 -0400

    Basic focus square implementation

---
+++ ios/RN/RNCameraFocusSquare.h
@@ -0,0 +1,17 @@
+//
+//  RNCameraFocusSquare.h
+//  Pods
+//
+//  Created by Jonathan Gilbert on 2021-10-01.
+//
+
+#ifndef RNCameraFocusSquare_h
+#define RNCameraFocusSquare_h
+
+#import <UIKit/UIKit.h>
+
+@interface RNCameraFocusSquare : UIView
+
+@end
+
+#endif /* RNCameraFocusSquare_h */
